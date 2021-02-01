eb1ee84de0b7c4d451a9e1529efa7a46a90a8c96
Steve Cline <steve.cline@weldre4.org>
2021-02-01 08:01:13 -0700

    Create Starter.ino

---
+++ Starter.ino
@@ -0,0 +1,23 @@
+// Create variables for the pins
+int redLED = 11;
+int greenLED = 10;
+int blueLED = 9;
+
+void setup(){
+  // Initialize pins as output devices
+  pinMode (redLED, );
+  pinMode (greenLED, );
+  pinMode (blueLED, );
+}
+
+void loop(){
+  // Red LED cycle
+  analogWrite (redLED, );     // full power
+  analogWrite (redLED, 100);  // 40% power
+  analogWrite (redLED, );     // no power
+
+  // Green LED cycle
+  
+  // Blue LED cycle
+
+}
